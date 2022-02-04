cfa300b83ec98a5a2d57a493e3d8e7ed99b0aa17
Pedro Henrick <phmc@ic.ufal.br>
2022-02-04 15:09:06 -0300

    Requisito 05 :ballot_box_with_check::headphones::notes:

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,7 +2,30 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      disable: true,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const NAME_MIN = 2;
+    const { name, value } = target;
+    if (value.length >= NAME_MIN) this.setState({ disable: false });
+    else this.setState({ disable: true });
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const {
+      search,
+      disable,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
@@ -10,12 +33,19 @@ class Search extends Component {
           <label htmlFor="search">
             <input
               type="text"
+              name="search"
+              data-testid="search-artist-input"
+              placeholder="Qual banda dejesa buscar?"
+              value={ search }
+              onChange={ this.onInputChange }
             />
           </label>
           <button
             type="button"
+            data-testid="search-artist-button"
+            disabled={ disable }
           >
-            Search
+            Pesquisar
           </button>
         </form>
       </div>
