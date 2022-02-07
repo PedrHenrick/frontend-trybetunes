1c7897ff1d50589fda7c8b9e7b7652d6b8553bfb
Pedro Henrick <phmc@ic.ufal.br>
2022-02-07 16:43:45 -0300

    Requisito 13 quase pronto... :headphones::notes:

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-// import ProfileEdit from './ProfileEdit';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userObj: {},
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const userObj = await getUser();
+    this.setState({ loading: false, userObj });
+  }
+
+  renderPage = () => {
+    const { userObj } = this.state;
+    return (
+      <div>
+        <img src={ userObj.image } alt={ userObj.name } data-testid="profile-image" />
+        <h3>{ `Nome: ${userObj.name}` }</h3>
+        <p>{ `Email: ${userObj.email}` }</p>
+        <p>{ `Descrição: ${userObj.description}` }</p>
+        <Link to="/profile/edit">Editar perfil</Link>
+      </div>
+    );
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { loading ? <Loading /> : this.renderPage() }
         <Link to="/search">Voltar</Link>
       </div>
     );
